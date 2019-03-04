1f63d5857a7f27d2dec097384b5fdc27787c9e71
erabhi <erabhishektiwary11@gmail.com>
2019-03-04 18:08:23 +0530

    ohs-demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import 'materialize-css/dist/css/materialize.min.css';
+import './styles/style.css';
+import logo from "./images/logo.png";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="container">
+        <div className="account-page">
+          <div className="login-page">
+            <div className="row">
+              <div className="logo">
+                <img src={logo} width="200px" height="150px" title="Logo" alt="Suraksha" />
+              </div>
+            </div>
+            <div className="row">
+              <div className="input-field col s12 form-groups relative">
+                <input placeholder="Username" id="userName" type="text" className="validate" />
+                <span className="username">user</span>
+              </div>
+            </div>
+            <div className="row margin-bottom-10">
+              <div className="input-field col s12 form-groups relative">
+                <input placeholder="Password" id="password" type="password" className="validate" />
+                <span className="password">password</span>
+              </div>
+            </div>
+            <div className="row">
+              <div className="form-groups col s12">
+                <button className="btn waves-effect waves-light green-btn" type="submit" name="action">login
+                </button>
+              </div>
+            </div>
+            <div className="form-groups">
+              <a className="forgot" href="javascript:void(0)">Forgot Username / Password?</a>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }

3b78783aa42190c68a90ea7c54cc1e59b6f83597
erabhi <erabhishektiwary11@gmail.com>
2019-03-04 18:26:24 +0530

    ohs-demo1

--- src/App.js
+++ src/App.js
@@ -5,6 +5,24 @@ import './styles/style.css';
 import logo from "./images/logo.png";
 
 class App extends Component {
+  state={
+    userName:'',
+    passWord:'',
+  }
+ 
+  handleUserName = e => this.setState({userName:e.target.value});
+  handlePassWord = e => this.setState({passWord:e.target.value});
+
+  handleLogin = e =>{
+    const {userName,passWord} = this.state
+    const payLoad = {
+      userName,
+      passWord:window.btoa(passWord),
+    }
+    //console.log(payLoad);
+    //api call to post payload
+  }
+
   render() {
     return (
       <div className="container">
@@ -17,19 +35,19 @@ class App extends Component {
             </div>
             <div className="row">
               <div className="input-field col s12 form-groups relative">
-                <input placeholder="Username" id="userName" type="text" className="validate" />
+                <input placeholder="Username" id="userName" type="text" className="validate" value={this.state.userName} onChange={this.handleUserName} />
                 <span className="username">user</span>
               </div>
             </div>
             <div className="row margin-bottom-10">
               <div className="input-field col s12 form-groups relative">
-                <input placeholder="Password" id="password" type="password" className="validate" />
+                <input placeholder="Password" id="password" type="password" className="validate" value={this.state.passWord} onChange={this.handlePassWord} />
                 <span className="password">password</span>
               </div>
             </div>
             <div className="row">
               <div className="form-groups col s12">
-                <button className="btn waves-effect waves-light green-btn" type="submit" name="action">login
+                <button className="btn waves-effect waves-light green-btn" type="submit" name="action" onClick={this.handleLogin}>login
                 </button>
               </div>
             </div>
